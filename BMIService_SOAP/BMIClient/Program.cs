77558fbb968ddd67d5eadfaa6bb18efa7d5459a2
hiroto0222 <aoyamahiroto0222@gmail.com>
2023-06-17 14:51:02 +0900

    add bmi soap api service reference

--- BMIService_SOAP/BMIClient/Program.cs
+++ BMIService_SOAP/BMIClient/Program.cs
@@ -13,79 +13,23 @@ namespace BMIClient
         {
             var weight = 140;
             var height = 67;
-            var api = new WebApiClient();
-            await api.GetMyBMI(height, weight);
-            await api.GetMyHealth(height, weight);
+
+            BMIServiceReference.BMIServiceSoapClient client = new BMIServiceReference.BMIServiceSoapClient();
+
+            Console.WriteLine("Call MyBMI");
+            double bmi = await client.MyBMIAsync(height, weight);
+            Console.WriteLine("bmi: " + bmi);
+
+            Console.WriteLine("Call MyHealth");
+            BMIServiceReference.MyHealthResponse res = await client.MyHealthAsync(height, weight);
+            BMIServiceReference.Health health = res.Body.MyHealthResult;
+            Console.WriteLine("bmi: " + health.bmi);
+            Console.WriteLine("risk: " + health.risk);
+            Console.WriteLine("more: " + string.Join(", ", health.more));
 
             // Wait for user input before exiting
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
-        }
-    }
-
-    public class WebApiClient
-    {
-        private readonly static HttpClient Client = new HttpClient();
-        private readonly string apiBaseURL = "https://localhost:44379/BMIService.asmx";
-
-        public async Task GetMyBMI(int height, int weight)
-        {
-            try
-            {
-                Console.WriteLine("Call MyBMI");
-
-                // Set SOAP request body
-                var soapRequest = $@"<?xml version=""1.0"" encoding=""utf-8""?>
-                                    <soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/""
-                                                   xmlns:tem=""http://tempuri.org/"">
-                                        <soap:Body>
-                                            <tem:MyBMI>
-                                                <tem:height>{height}</tem:height>
-                                                <tem:weight>{weight}</tem:weight>
-                                            </tem:MyBMI>
-                                        </soap:Body>
-                                    </soap:Envelope>";
-                var content = new StringContent(soapRequest, Encoding.UTF8, "text/xml");
-
-                var res = await Client.PostAsync(apiBaseURL, content);
-                var resBody = await res.Content.ReadAsStringAsync();
-                Console.WriteLine(resBody);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            Console.WriteLine("End");
-        }
-
-        public async Task GetMyHealth(int height, int weight)
-        {
-            try
-            {
-                Console.WriteLine("Call MyHealth");
-
-                // Set SOAP request body
-                var soapRequest = $@"<?xml version=""1.0"" encoding=""utf-8""?>
-                                    <soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/""
-                                                   xmlns:tem=""http://tempuri.org/"">
-                                        <soap:Body>
-                                            <tem:MyHealth>
-                                                <tem:height>{height}</tem:height>
-                                                <tem:weight>{weight}</tem:weight>
-                                            </tem:MyHealth>
-                                        </soap:Body>
-                                    </soap:Envelope>";
-                var content = new StringContent(soapRequest, Encoding.UTF8, "text/xml");
-
-                var res = await Client.PostAsync(apiBaseURL, content);
-                var resBody = await res.Content.ReadAsStringAsync();
-                Console.WriteLine(resBody);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            Console.WriteLine("End");
         }
     }
 }
