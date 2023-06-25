07937023b327dee3c55adf57f2dd8938ccec8371
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-06-25 21:41:27 +0900

    add basic api call

--- BMI Client App/BMI Client App/ContentView.swift
+++ BMI Client App/BMI Client App/ContentView.swift
@@ -8,15 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var height = 0.0
+    @State private var weight = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("BMI API")
+                .font(.title)
         }
         .padding()
     }
+    
+    func getHealth() async throws -> Health {
+        let endpoint = "http://webstrar99.fulton.asu.edu/page8/Service1.svc/calculateBMI?"
+        
+        guard let url = URL(string: endpoint)
+        else {
+            throw GHError.invalidURL
+        }
+        
+        let (data, res) = try await URLSession.shared.data(from: url)
+        
+        guard let res = res as? HTTPURLResponse, res.statusCode == 200
+        else {
+            throw GHError.invalidResponse
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(Health.self, from: data)
+        } catch {
+            throw GHError.invalidData
+        }
+    }
+}
+
+struct Health: Codable {
+    let bmi: Double
+    let risk: String
+    let more: [String]
+}
+
+enum GHError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
 }
 
 struct ContentView_Previews: PreviewProvider {
