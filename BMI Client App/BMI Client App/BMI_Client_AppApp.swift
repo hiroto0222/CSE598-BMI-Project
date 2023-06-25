6a126013162508312ee5a50c91a14962fce07c23
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-06-25 18:19:37 +0900

    init swift project

---
+++ BMI Client App/BMI Client App/BMI_Client_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_Client_AppApp.swift
+//  BMI Client App
+//
+//  Created by Hiroto Aoyama on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct BMI_Client_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
